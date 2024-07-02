26079fbb0850c130cb33aea4c736c0e42c0703b0
Leshan21 <leshanpasindu12@gmail.com>
2024-07-02 19:24:24 +0530

    write-somthing-async

--- Lec-003/app.js
+++ Lec-003/app.js
@@ -3,11 +3,14 @@ const fs = require('fs');
 
 fs.readFile('./files/start.txt', 'utf-8', (error1, data1) => {
   console.log(data1)
-
-  //const filename = data1.trim();
-
   fs.readFile(`./files/${data1}.txt`, 'utf-8', (error2, data2) => {
     console.log(data2);
+    fs.readFile('./files/append.txt', 'utf-8', (error3, data3) => {
+      console.log(data3);
+      fs.writeFile('./files/output.txt',`${data2}\n\n${data3}\n\nData created ${new Date()}`, () => {
+        console.log("File written successfully....!!!");
+      });
+    })
   })
 })
 console.log("Reading Files......");
