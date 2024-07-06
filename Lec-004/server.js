68cd92f59697f392a5d5ee2f0956dc465eab951e
Leshan21 <leshanpasindu12@gmail.com>
2024-07-06 22:14:46 +0530

    update

--- Lec-004/server.js
+++ Lec-004/server.js
@@ -4,9 +4,10 @@ const http = require('http');
 //create server
 const server = http.createServer((request, response) => {
   console.log('A new request reveive....');
+  response.end('Hello World!\n');
 });
 
 //start sever
-server.listen(8000, '127.0.0.1',() => {
+server.listen(8000,() => {
     console.log('server started....');
 });
